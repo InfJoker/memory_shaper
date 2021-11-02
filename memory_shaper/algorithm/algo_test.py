2fb08f5f3dd5fca51eb2c2fdddd10ae407235da5
artyomVB <79898892+artyomVB@users.noreply.github.com>
2021-11-02 14:01:01 +0300

    Correct tests

--- memory_shaper/algorithm/algo_test.py
+++ memory_shaper/algorithm/algo_test.py
@@ -3,22 +3,23 @@ from datetime import timedelta, datetime
 import FlashCardAlgo
 
 
-class TestAlgo:
-    def test_count_time_before_next_show_false(self):
-        f = FlashCardAlgo.FlashCardAlgorithm()
-        prev_delta = f.current_delta
-        time_d = f.count_time_before_next_show(False)
-        assert(f.current_delta < prev_delta)
-        assert(time_d == timedelta(minutes=2) or time_d == timedelta(minutes=f.current_delta * 10))
+def test_count_time_before_next_show_false():
+    f = FlashCardAlgo.FlashCardAlgorithm()
+    prev_delta = f.current_delta
+    time_d = f.count_time_before_next_show(False)
+    assert(f.current_delta < prev_delta)
+    assert(time_d == timedelta(minutes=2) or time_d == timedelta(minutes=f.current_delta * 10))
 
-    def test_count_time_before_next_show_true(self):
-        f = FlashCardAlgo.FlashCardAlgorithm()
-        prev_delta = f.current_delta
-        time_d = f.count_time_before_next_show(True)
-        assert(f.current_delta > prev_delta)
-        assert(time_d == timedelta(minutes=2) or time_d == timedelta(minutes=f.current_delta * 10))
 
-    def test_reset_time(self):
-        f = FlashCardAlgo.FlashCardAlgorithm()
-        f.reset_time(True)
-        assert(datetime.now() < f.next_show)
+def test_count_time_before_next_show_true():
+    f = FlashCardAlgo.FlashCardAlgorithm()
+    prev_delta = f.current_delta
+    time_d = f.count_time_before_next_show(True)
+    assert(f.current_delta > prev_delta)
+    assert(time_d == timedelta(minutes=2) or time_d == timedelta(minutes=f.current_delta * 10))
+
+
+def test_reset_time():
+    f = FlashCardAlgo.FlashCardAlgorithm()
+    f.reset_time(True)
+    assert(datetime.now() < f.next_show)
