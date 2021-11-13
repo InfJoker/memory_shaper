1c48c431c64f7342a332afdba08c42c72f716cea
Heorhi Hamezardashvili <gogam@yandex-team.ru>
2021-11-13 14:17:06 +0300

    Add functions to init user cards for particular user
    This can be done in other handler too. Now it is in /login just to be compatible functionality

--- memory_shaper/handlers/card_switcher.py
+++ memory_shaper/handlers/card_switcher.py
@@ -1,9 +1,15 @@
+from typing import List, Optional
+
 from memory_shaper.algorithm.FlashCardAlgo import get_modified_card
 from memory_shaper.tmp_cards import CARDS, init_queue, get_queue
 
 from flask import render_template, redirect, url_for, request, session
 
 from app import app
+from memory_shaper.app import new_sql_session
+from sqlalchemy.orm import Session
+
+from memory_shaper import models
 
 
 @app.route('/')
@@ -19,6 +25,17 @@ def login():
             return redirect(url_for('login'))
         else:
             session['login'] = request.form['login']
+            sql_session = new_sql_session()
+            user = (
+                sql_session.query(models.User).filter_by(login=session['login']).one_or_none()
+            )  # type: Optional[models.User]
+
+            session['user_nickname'] = user.nickname
+
+            if user is None:
+                return redirect(url_for('login'))
+
+            update_user_decks(sql_session, user)
             return redirect(url_for('card'))
     return render_template('login.html')
 
@@ -40,3 +57,29 @@ def check_answer():
     CARDS[i] = get_modified_card(CARDS[i], request.form['button'] == 'Correct')
     queue.put((CARDS[i].get_next_show_time(), i))
     return redirect(url_for('card'))
+
+
+def update_user_decks(sql_session: Session, user: models.User) -> None:
+    user_decks = (
+        sql_session.query(models.UserDeck).filter_by(user_nickname=user.nickname).all()
+    )  # type: List[models.UserDeck]
+    for user_deck in user_decks:
+        fill_user_deck(sql_session, user_deck)
+
+
+def fill_user_deck(sql_session: Session, user_deck: models.UserDeck) -> int:
+    user_cards = (
+        sql_session.query(models.UserCard).filter_by(deck_id=user_deck.deck_id).all()
+    )  # type: List[models.UserCard]
+    cards_in_deck_ids = [user_card.card_id for user_card in user_cards]
+    cards_to_fill = (
+        sql_session.query(models.Card)
+        .filter_by(deck_id=user_deck.deck_id)
+        .filter(models.Card.id.not_in(cards_in_deck_ids))
+        .all()
+    )  # type: List[models.Card]
+    for card in cards_to_fill:
+        instance = models.UserCard(user=user_deck.user, deck_id=user_deck.deck_id, card=card)
+        sql_session.add(instance)
+    sql_session.commit()
+    return len(cards_to_fill)
